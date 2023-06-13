7e5469b699b0b2dd00171fbf7340d84f85e3443e
Antonio BITOUNIS <antonio.bitounis@rbinternational.com>
2023-06-13 11:33:05 +0200

    added H2 TCP server (in-mem dbs)

--- order-service/src/main/java/at/antonio/saga/order/OrderServiceApp.java
+++ order-service/src/main/java/at/antonio/saga/order/OrderServiceApp.java
@@ -1,11 +1,20 @@
 package at.antonio.saga.order;
 
+import org.h2.tools.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.sql.SQLException;
 
 @SpringBootApplication
 public class OrderServiceApp {
   public static void main(String[] args) {
     SpringApplication.run(OrderServiceApp.class);
+  }
+
+  @Bean(initMethod = "start", destroyMethod = "stop")
+  public Server h2Server() throws SQLException {
+    return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "8085");
   }
 }
--- payment-service/src/main/java/at/antonio/saga/payment/PaymentServiceApp.java
+++ payment-service/src/main/java/at/antonio/saga/payment/PaymentServiceApp.java
@@ -1,11 +1,20 @@
 package at.antonio.saga.payment;
 
+import org.h2.tools.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.sql.SQLException;
 
 @SpringBootApplication
 public class PaymentServiceApp {
     public static void main(String[] args){
         SpringApplication.run(PaymentServiceApp.class);
+    }
+
+    @Bean(initMethod = "start", destroyMethod = "stop")
+    public Server h2Server() throws SQLException {
+        return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "8086");
     }
 }
